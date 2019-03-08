5be0bd89d9a5960e947183b4bca9c577567bf86e
yttrium7 <yttrium.l@pm.me>
2019-03-08 17:53:28 +0800

    update

--- config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    port: 3000,
-    mongodb:'mongodb://Yttrium:cHENNIGHT7007@ds159129.mlab.com:59129/paprica',
-    cloud_name: 'paprica',
-    api_key: '422941162968164',
-    api_secret: 'PEWn3D9LRlI4ORISOWZvXBFShKo'
-};
